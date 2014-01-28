8109bf6326e75caa2286b67888dd75148c17dd84
Robert Rouse <robert@theymaybecoders.com>
2014-01-28 12:06:35 -0600

    Update ResourceWeek view

--- src/resource/ResourceWeekView.js
+++ src/resource/ResourceWeekView.js
@@ -6,48 +6,33 @@ function ResourceWeekView(element, calendar) {
 
 
     // exports
+    t.incrementDate = incrementDate;
     t.render = render;
 
     // imports
     ResourceView.call(t, element, calendar, 'resourceWeek');
-    var opt = t.opt;
-    var renderBasic = t.renderBasic;
-    var formatDates = calendar.formatDates;
     var getResources = t.getResources;
 
+    function incrementDate(date, delta) {
+        return date.clone().stripTime().add('weeks', delta).startOf('week');
+    }
 
-    function render(date, delta) {
-        if (delta === 100 || delta === -100) {
-            // 100 means we want to skip full week (largePrev/largeNext pressed)
-            var start = addDays(date, (delta > 0 ? 7 : -7), false);
-            var end = addDays(cloneDate(start), 7);
-        }
-        else if (delta) {
-            var start = addDays(t.visStart, delta * opt('paginateResourceWeek'), false);
-            var end = addDays(cloneDate(start), 7);
-        }
-        else {
-            date = new Date();
-            var start = addDays(cloneDate(date, true), -((date.getDay() - opt('firstDay') + 7) % 7));
-            var end = addDays(cloneDate(start), 7);
-        }
 
-        var visStart = cloneDate(start);
-        var visEnd = cloneDate(end);
-        var weekends = opt('weekends');
-        if (!weekends) {
-            skipWeekend(visStart);
-            skipWeekend(visEnd, -1, true);
-        }
-        t.title = formatDates(
-            visStart,
-            addDays(cloneDate(visEnd), -1),
-            opt('titleFormat')
+    function render(date) {
+
+        t.intervalStart = date.clone().stripTime().startOf('week');
+        t.intervalEnd = t.intervalStart.clone().add('weeks', 1);
+
+        t.start = t.skipHiddenDays(t.intervalStart);
+        t.end = t.skipHiddenDays(t.intervalEnd, -1, true);
+
+        t.title = calendar.formatRange(
+            t.start,
+            t.end.clone().subtract(1), // make inclusive by subtracting 1 ms
+            t.opt('titleFormat'),
+            ' \u2014 ' // emphasized dash
         );
-        t.start = start;
-        t.end = end;
-        t.visStart = visStart;
-        t.visEnd = visEnd;
-        renderBasic(getResources.length, weekends ? 7 : 5, false);
+
+        t.renderBasic(getResources.length, t.getCellsPerWeek(), false);
     }
 }
